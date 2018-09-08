81e40e5f3238be1e27c1344f0ae69b9a907554cc
abtom61 <a.samed.app@outlook.de>
2018-09-09 00:40:58 +0200

    revert

--- knudu/App.js
+++ knudu/App.js
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Sameasdd</Text>
+        <Text style={styles.welcome}>Revert</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
