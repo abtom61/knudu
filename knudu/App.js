15c5c8a146fa3ab7dc789e7fe2ac9e969c180bd7
Süleyman <s.balci.app@gmail.com>
2018-09-09 14:24:47 +0200

    asd

--- knudu/App.js
+++ knudu/App.js
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Samed war hier</Text>
+        <Text style={styles.welcome}>Samed war hier... und jetzt bin ich hier</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
