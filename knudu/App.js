7022520c29046b8fc35d6091d7f079d60a17b8f9
abtom61 <a.samed.app@outlook.de>
2018-09-11 22:20:01 +0200

    11.09.2018 A.Samed - Login Page Gui verbessert

--- knudu/App.js
+++ knudu/App.js
@@ -59,7 +59,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ecf0f1',
+    backgroundColor: '#bdc3c7',
   },
   ueberschrift:{
     fontSize: 30,
@@ -83,4 +83,5 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     marginBottom: 10,
   },
+ 
 });
